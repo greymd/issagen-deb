1850ff22c7d19a48a957ac31fc95f89c3f825682
greymd <greengregson@gmail.com>
2019-01-13 18:35:33 +0000

    Remove unnecessary file

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-printf "VERSION: "
-read VERSION < /dev/tty
-cp -rL issagen-template issagen-${VERSION}
-cd issagen-${VERSION}
-debuild -S -sd
-cd ..
-dput ppa:issagen/ppa issagen_${VERSION}*_source.changes
-git ls-files --others --directory | xargs rm -rf
